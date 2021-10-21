85871a87b2172e884b3c465a9f927f24f98edde7
Erdodif <erdodif@gmail.com>
2021-10-21 11:35:39 +0200

    UML 3 - Számla absztrakt osztály létrehozása

--- src/hu/petrik/bank/szolgaltatasok/Szamla.java
+++ src/hu/petrik/bank/szolgaltatasok/Szamla.java
@@ -1,4 +1,22 @@
 package hu.petrik.bank.szolgaltatasok;
 
-public class Szamla {
+import hu.petrik.bank.felhasznalo.Tulajdonos;
+
+public abstract class Szamla extends BankiSzolgaltatas {
+    protected int aktualisEgyenleg;
+
+    public Szamla(Tulajdonos tulajdonos) {
+        super(tulajdonos);
+        aktualisEgyenleg = 0;
+    }
+
+    public int getAktualisEgyenleg() {
+        return aktualisEgyenleg;
+    }
+
+    public void befizet(int osszeg) {
+        aktualisEgyenleg += osszeg;
+    }
+
+    public abstract boolean kivesz(int osszeg);
 }
