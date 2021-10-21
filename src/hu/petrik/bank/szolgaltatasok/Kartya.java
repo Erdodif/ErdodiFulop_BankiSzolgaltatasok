c643bd017704ac7e73bbee8b175c48fa7dad461e
Erdodif <erdodif@gmail.com>
2021-10-21 11:47:52 +0200

    UML 6 - Kartya osztály elkészítése

--- src/hu/petrik/bank/szolgaltatasok/Kartya.java
+++ src/hu/petrik/bank/szolgaltatasok/Kartya.java
@@ -1,4 +1,22 @@
 package hu.petrik.bank.szolgaltatasok;
 
-public class Kartya {
+import hu.petrik.bank.felhasznalo.Tulajdonos;
+
+public final class Kartya extends BankiSzolgaltatas{
+    private final Szamla szamla;
+    private final String kartyaSzam;
+
+    public Kartya(Tulajdonos tulajdonos, Szamla szamla, String kartyaSzam) {
+        super(tulajdonos);
+        this.szamla = szamla;
+        this.kartyaSzam = kartyaSzam;
+    }
+
+    public String getKartyaSzam() {
+        return kartyaSzam;
+    }
+
+    public boolean vasarlas(int osszeg){
+        return szamla.kivesz(osszeg);
+    }
 }
