f094ca385bea1389f656bcb36da7154108058967
Erdodif <erdodif@gmail.com>
2021-10-21 12:37:59 +0200

    Teszt a Main osztályban (jelenleg még sikertelen

--- src/hu/petrik/bank/Bank.java
+++ src/hu/petrik/bank/Bank.java
@@ -11,48 +11,48 @@ import java.util.List;
 public final class Bank {
     private final List<Szamla> szamlaLista;
 
-    public Bank(int max){
+    public Bank(int max) {
         szamlaLista = new ArrayList<>(max);
     }
 
-    public Szamla szamlaNyitas(Tulajdonos tulajdonos, int hitelKeret){
+    public Szamla szamlaNyitas(Tulajdonos tulajdonos, int hitelKeret) {
         Szamla szamla;
-        if (hitelKeret == 0){
+        if (hitelKeret == 0) {
             szamla = szamlaNyitas(tulajdonos);
+        } else {
+            szamla = new HitelSzamla(tulajdonos, hitelKeret);
         }
-        else{
-            szamla = szamlaNyitas(tulajdonos);
-        }
+        szamlaLista.add(szamla);
         return szamla;
     }
 
-    public Szamla szamlaNyitas(Tulajdonos tulajdonos){
+    public MegtakaritasiSzamla szamlaNyitas(Tulajdonos tulajdonos) {
         return new MegtakaritasiSzamla(tulajdonos);
     }
 
-    public int getOsszegyenleg(){
+    public int getOsszegyenleg() {
         int szum = 0;
-        for(Szamla szamla: szamlaLista){
+        for (Szamla szamla : szamlaLista) {
             szum += szamla.getAktualisEgyenleg();
         }
         return szum;
     }
 
-    public Szamla getLegnagyobbEgyenleguSzamla(){
+    public Szamla getLegnagyobbEgyenleguSzamla() {
         Szamla szamlaMaistro = szamlaLista.get(0);
-        for(Szamla szamla: szamlaLista){
-            if (szamla.getAktualisEgyenleg()>szamlaMaistro.getAktualisEgyenleg()){
+        for (Szamla szamla : szamlaLista) {
+            if (szamla.getAktualisEgyenleg() > szamlaMaistro.getAktualisEgyenleg()) {
                 szamlaMaistro = szamla;
             }
         }
         return szamlaMaistro;
     }
 
-    public long getOsszhitelkeret(){
+    public long getOsszhitelkeret() {
         int szum = 0;
-        for(Szamla szamla: szamlaLista){
-            if(szamla.getClass()== HitelSzamla.class){
-                szum += ((HitelSzamla)szamla).getHitelKeret();
+        for (Szamla szamla : szamlaLista) {
+            if (szamla.getClass() == HitelSzamla.class) {
+                szum += ((HitelSzamla) szamla).getHitelKeret();
             }
         }
         return szum;
--- src/hu/petrik/bank/Main.java
+++ src/hu/petrik/bank/Main.java
@@ -1,8 +1,48 @@
 package hu.petrik.bank;
 
+import hu.petrik.bank.felhasznalo.Tulajdonos;
+import hu.petrik.bank.szolgaltatasok.Szamla;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
+    static Random random = new Random();
+    static List<Tulajdonos> tulajdonosok = new ArrayList<>();
+
+    private static void bankUgyintezes(Bank bank, int meret) {
+        for (int i = 0; i < meret; i++) {
+            int randomindex = random.nextInt(tulajdonosok.size());
+            bank.szamlaNyitas(tulajdonosok.get(randomindex),
+                    random.nextBoolean() ? random.nextInt(100000) : 0);
+        }
+    }
+
+    private static String Kiirato(Bank bank) {
+        String ki = "Összegyenleg: ";
+        ki += bank.getOsszegyenleg();
+        ki += "\nÖsszhitelkeret: ";
+        ki += bank.getOsszhitelkeret();
+        ki += "\nLegbőbb számla: ";
+        ki += bank.getLegnagyobbEgyenleguSzamla().getAktualisEgyenleg();
+        ki += "\nTulajdonosa: ";
+        ki += bank.getLegnagyobbEgyenleguSzamla().getTulajdonos().getNev();
+        return ki;
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        Bank raifaisen = new Bank(10);
+        Bank otp = new Bank(20);
+        tulajdonosok.add(new Tulajdonos("Én"));
+        tulajdonosok.add(new Tulajdonos("Te"));
+        tulajdonosok.add(new Tulajdonos("Ő"));
+        tulajdonosok.add(new Tulajdonos("Mi"));
+        tulajdonosok.add(new Tulajdonos("Ti"));
+        tulajdonosok.add(new Tulajdonos("Ők"));
+        bankUgyintezes(raifaisen, 10);
+        bankUgyintezes(otp, 20);
+        System.out.println("\nOtp: \n"+Kiirato(otp));
+        System.out.println("\nRaifaisen: \n"+Kiirato(raifaisen));
     }
 }
