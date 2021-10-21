c05d3f47ec2b420828d2c05041f0a344c1823713
Erdodif <erdodif@gmail.com>
2021-10-21 11:35:58 +0200

    UML 4 - HitelSzamla osztály létrehozása

--- src/hu/petrik/bank/szolgaltatasok/HitelSzamla.java
+++ src/hu/petrik/bank/szolgaltatasok/HitelSzamla.java
@@ -1,4 +1,26 @@
 package hu.petrik.bank.szolgaltatasok;
 
-public class HitelSzamla {
+import hu.petrik.bank.felhasznalo.Tulajdonos;
+
+public final class HitelSzamla extends Szamla {
+    private final int hitelKeret;
+
+    public HitelSzamla(Tulajdonos tulajdonos, int hitelKeret) {
+        super(tulajdonos);
+        this.hitelKeret = hitelKeret;
+    }
+
+    public int getHitelKeret() {
+        return hitelKeret;
+    }
+
+    @Override
+    public boolean kivesz(int osszeg) {
+        boolean siker = false;
+        if(getAktualisEgyenleg() - osszeg > -hitelKeret){
+            siker = true;
+            aktualisEgyenleg-=osszeg;
+        }
+        return siker;
+    }
 }
