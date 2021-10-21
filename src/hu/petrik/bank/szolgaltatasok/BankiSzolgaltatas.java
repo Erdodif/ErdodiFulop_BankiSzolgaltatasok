5f508771eeea743ab67a04c73455df69aebc2d23
Erdodif <erdodif@gmail.com>
2021-10-21 11:14:02 +0200

    UML 2 - BankiSzolgaltatas absztrakt osztály elkészítése

--- src/hu/petrik/bank/szolgaltatasok/BankiSzolgaltatas.java
+++ src/hu/petrik/bank/szolgaltatasok/BankiSzolgaltatas.java
@@ -1,4 +1,15 @@
 package hu.petrik.bank.szolgaltatasok;
 
-public class BankiSzolgaltatas {
+import hu.petrik.bank.felhasznalo.Tulajdonos;
+
+public abstract class BankiSzolgaltatas {
+    private Tulajdonos tulajdonos;
+
+    protected BankiSzolgaltatas(Tulajdonos tulajdonos) {
+        this.tulajdonos = tulajdonos;
+    }
+
+    public Tulajdonos getTulajdonos() {
+        return tulajdonos;
+    }
 }
