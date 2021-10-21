f0bdc8e3d2959ab627bd3857c852888633615599
Erdodif <erdodif@gmail.com>
2021-10-21 11:38:51 +0200

    UML 5 - MegtakaritasiSzamla osztály létrehozása

--- src/hu/petrik/bank/szolgaltatasok/MegtakaritasiSzamla.java
+++ src/hu/petrik/bank/szolgaltatasok/MegtakaritasiSzamla.java
@@ -1,4 +1,36 @@
 package hu.petrik.bank.szolgaltatasok;
 
-public class MegtakaritasiSzamla {
+import hu.petrik.bank.felhasznalo.Tulajdonos;
+
+public final class MegtakaritasiSzamla extends Szamla{
+    private double kamat;
+    public static double alapKamat;
+
+    public MegtakaritasiSzamla(Tulajdonos tulajdonos) {
+        super(tulajdonos);
+        kamat = 0;
+    }
+
+    public double getKamat() {
+        return kamat;
+    }
+
+    public void setKamat(int kamat) {
+        this.kamat = kamat;
+    }
+
+    @Override
+    public boolean kivesz(int osszeg) {
+        boolean siker = false;
+        if(getAktualisEgyenleg() - osszeg > 0){
+            siker = true;
+            aktualisEgyenleg-=osszeg;
+        }
+        return siker;
+    }
+
+    public void kamatJovairas(){
+        aktualisEgyenleg += kamat;
+        kamat = 0;
+    }
 }
